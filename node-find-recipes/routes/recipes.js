b1bdcaa6f7da21c133e81c93cf165c6269b85f7c
Christian Tapia <christian1020_94@hotmail.com>
2021-11-06 14:33:47 -0500

    node-find-recipes solved

--- node-find-recipes/routes/recipes.js
+++ node-find-recipes/routes/recipes.js
@@ -1,5 +1,21 @@
-var recipes = require('../recipes.json');
-var router = require('express').Router();
+var recipes = require("../recipes.json");
+var router = require("express").Router();
+
+router.get("/step/:id", (req, res, next) => {
+  const recipe = recipes.find((x) => x.id == req.params.id);
+  if (isNaN(req.params.id)) {
+    return res.status(200).send("NOT_FOUND");
+  }
+
+  if (recipe) {
+    const elapsedTime = req.query.elapsedTime || 0;
+    const i = recipe.timers.findIndex((x) => x >= elapsedTime);
+
+    console.log(i);
+    return res.status(200).json({ index: i });
+  } else {
+    return res.status(400).send("NOT_FOUND");
+  }
+});
 
 module.exports = router;
-
