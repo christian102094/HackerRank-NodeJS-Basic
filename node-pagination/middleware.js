e67ed8ae2350bba03518ce8f61770db9d96950cf
Christian Tapia <christian1020_94@hotmail.com>
2021-11-06 15:01:55 -0500

    pagination solved

--- node-pagination/middleware.js
+++ node-pagination/middleware.js
@@ -1,13 +1,12 @@
 module.exports = (req, res, next) => {
-  const { page = 1, limit = 3, q = '' } = req.query;
+  const { page = 1, limit = 3, q = "" } = req.query;
 
-  console.log("query", q);
   req.context = {
     page: +page,
     limit: +limit,
     skip: (page - 1) * limit,
     searchTerm: q,
-    search: new RegExp(q, "gi")
+    search: new RegExp(req.query.q, "gi"),
   };
   next();
 };
